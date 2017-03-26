3e9976838a955f49436c4939ec43b32e0398e300
Kaicheng <kwang1993@g.ucla.edu>
2017-03-26 16:02:29 -0700

    first
    first

---
+++ getFiles.sh
@@ -0,0 +1,9 @@
+#wget http://babylon.library.ucla.edu/~broadwell/fakenews/bridgegate/ARCHIVEIT-8424-ONE_TIME-JOB260453-20170113223854038-00000.warc.gz	
+#wget http://babylon.library.ucla.edu/~broadwell/fakenews/bridgegate/ARCHIVEIT-8424-ONE_TIME-JOB260736-20170116092423279-00001.warc.gz
+#wget http://babylon.library.ucla.edu/~broadwell/fakenews/bridgegate/ARCHIVEIT-8424-ONE_TIME-JOB260736-20170116093258354-00002.warc.gz
+#wget http://babylon.library.ucla.edu/~broadwell/fakenews/bridgegate/ARCHIVEIT-8424-ONE_TIME-JOB260736-20170116125225105-00003.warc.gz
+#wget http://babylon.library.ucla.edu/~broadwell/fakenews/bridgegate/ARCHIVEIT-8424-ONE_TIME-JOB260736-20170117090132777-00004.warc.gz
+#wget http://babylon.library.ucla.edu/~broadwell/fakenews/bridgegate/ARCHIVEIT-8424-ONE_TIME-JOB260736-20170118033455765-00005.warc.gz
+#wget http://babylon.library.ucla.edu/~broadwell/fakenews/bridgegate/ARCHIVEIT-8424-ONE_TIME-JOB260736-20170118204012997-00006.warc.gz
+#wget http://babylon.library.ucla.edu/~broadwell/fakenews/bridgegate/ARCHIVEIT-8424-ONE_TIME-JOB260736-20170116085642516-00000.warc.gz
+
